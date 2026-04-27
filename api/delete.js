7d200ddcd81f7a83f927946d17289ad2d97b6171
Ayoub Lahdoud <118694667+ayoublahdoud@users.noreply.github.com>
2026-04-27 17:22:02 +0100

    Update delete.js

--- api/delete.js
+++ api/delete.js
@@ -1,4 +1,4 @@
-import { Redis } from '@upstash/redis';
+const { Redis } = require('@upstash/redis');
 const redis = new Redis({ url: process.env.UPSTASH_REDIS_REST_URL, token: process.env.UPSTASH_REDIS_REST_TOKEN });
 
 export default async function handler(req, res) {
